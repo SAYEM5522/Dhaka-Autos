cda1ccc01a9ff7a7bbc1e08734f5248dc95a70ff
Asif Sorowar <sorowar.asif@gmail.com>
2024-10-15 17:11:32 +0600

    added header and footer as common component

--- src/component/contact-us/Contact.js
+++ src/component/contact-us/Contact.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Header from "../header/Header";
-import Footer from "../footer/Footer";
 import Banner from "../Banner/Banner";
 import ContactList from "./ContactList";
 
 const Contact = () => {
   return (
     <div>
-      <Header />
       <Banner url={"contact.png"} />
-      <ContactList/>
-      <Footer />
+      <ContactList />
     </div>
   );
 };
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../header/Header";
-import Footer from "../footer/Footer";
 import Hero from "../hero/Hero";
 import Why from "../why/Why";
 import Goal from "../goal/Goal";
@@ -10,12 +8,10 @@ import CallToAction from "../call-to-action/CallToAction";
 const Home = () => {
   return (
     <div>
-      <Header />
       <Hero />
       <Goal />
       <Why />
       <CallToAction />
-      <Footer />
     </div>
   );
 };
