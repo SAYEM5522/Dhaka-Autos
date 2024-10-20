4cfae2ee0f91bd5ea6966dd1e2b2292ac674d025
Asif Sorowar <sorowar.asif@gmail.com>
2024-10-20 17:12:52 +0600

    homepage redesigned

--- src/component/brand/Brand.js
+++ src/component/brand/Brand.js
@@ -1,152 +1,49 @@
 import React from "react";
 
+const brands = [
+  { name: "Toyota", image: "toyota.png" },
+  { name: "Toyota", image: "honda.png" },
+  { name: "Toyota", image: "mitsubishi.png" },
+  { name: "Toyota", image: "nissan.png" },
+  { name: "Toyota", image: "lexus.png" },
+  { name: "Toyota", image: "subaru.png" },
+  { name: "Toyota", image: "audi.png" },
+  { name: "Toyota", image: "benze.png" },
+  { name: "Toyota", image: "bmw.png" },
+  { name: "Toyota", image: "porsche.png" },
+  { name: "Toyota", image: "land.png" },
+  { name: "Toyota", image: "mazda.png" },
+  { name: "Toyota", image: "ford.png" },
+  { name: "Toyota", image: "volk.png" },
+  { name: "Toyota", image: "jeep.png" },
+];
+
 const Brand = () => {
   return (
-    <div >
-      <section   className="py-12 sm:py-16 lg:py-20">
-        <div className="mx-auto max-w-7xl ">
-          <div className="max-w-xl mt-8 mx-auto text-center xl:max-w-3xl">
-            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">
-            Buy/Pre - Order Your Vehicle Now 
-            </h2>
-            <p className="text-2xl mt-2">Choose Your Brand</p>
-          </div>
+    <div className="">
+      <div className="mx-auto">
+        <div className="max-w-xl mt-8 mx-auto text-center lg:max-w-3xl">
+          <h2 className="text-3xl font-bold text-gray-900 lg:text-5xl font-pj">
+            Buy/Pre - Order Your Vehicle Now
+          </h2>
+          <p className="text-2xl lg:text-3xl mt-2">Choose Your Brand</p>
         </div>
+      </div>
 
-        <div className="mt-9 lg:mt-20">
-          <div className="-my-4">
-            <div className="w-full py-6 overflow-hidden">
-              <div className="grid grid-cols-3 gap-5 text-center lg:grid-cols-5 lg:gap-6 lg:-mx-8 xl:gap-4">
-                <div className="!h-[150px] bg-white  shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="toyota.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="honda.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="mitsubishi.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="nissan.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="lexus.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="subaru.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="audi.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="benze.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-centerw-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="bmw.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="porsche.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="land.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="mazda.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="ford.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="volk.png" />
-                    </div>
-                    
-                  </div>
-                </div>
-                <div className="!h-[150px] bg-white shadow-lg  items-center justify-center flex">
-                  <div className="items-center justify-center flex">
-                    <div className="inline-flex items-center justify-center w-14 h-14 mx-auto rounded-full sm:w-20 sm:h-20 ">
-                      <img src="jeep.png" />
-                    </div>
-                    
-                  </div>
-                </div>
+      <div className="mt-14">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 text-center lg:grid-cols-5 lg:gap-6 xl:gap-4">
+          {brands.map((brand) => (
+            <div
+              key={brand.image}
+              className="h-[200px] w-auto bg-white  shadow-lg  items-center justify-center flex"
+            >
+              <div className="inline-flex items-center justify-center w-[80%] h-[80%] rounded-full">
+                <img src={brand.image} />
               </div>
             </div>
-          </div>
+          ))}
         </div>
-      </section>
+      </div>
     </div>
   );
 };
--- src/component/call-to-action/CallToAction.js
+++ src/component/call-to-action/CallToAction.js
@@ -2,41 +2,36 @@ import React from "react";
 
 const CallToAction = () => {
   return (
-    <div>
-      <section className="relative py-12  sm:py-16 lg:py-20 xl:py-32">
-        <div className="absolute inset-0">
-          <img
-            className="object-cover w-full h-full"
-            src="call.jpg"
-            alt=""
-          />
-        </div>
+    <section className="relative py-16 lg:py-20 xl:py-32">
+      <div className="absolute inset-0">
+        <img className="object-cover w-full h-full" src="call.jpg" alt="" />
+      </div>
 
-        {/* <div className="absolute inset-0 bg-gray-900/50"></div> */}
+      {/* <div className="absolute inset-0 bg-gray-900/50"></div> */}
 
-        <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-          <div className="max-w-xl mx-auto text-center">
-            <h2 className="text-3xl font-bold text-white sm:text-4xl lg:text-5xl">
+      <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="max-w-xl mx-auto text-center">
+          <h2 className="text-3xl font-bold text-white sm:text-4xl lg:text-6xl">
             Get Latest Car
-            </h2>
-            <p className="mt-4 text-base font-normal leading-7 text-white sm:mt-6">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.
-            </p>
+          </h2>
+          <p className="mt-7 text-xl md:text-2xl font-normal leading-8 text-white">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis.
+          </p>
 
-            <div className="mt-8 sm:mt-10">
-              <a
-                href="#"
-                title=""
-                className="inline-flex items-center justify-center px-8 py-3 text-base font-bold leading-7 text-gray-900 transition-all duration-200 bg-white border border-transparent rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
-                role="button"
-              >
-                Explore Now
-              </a>
-            </div>
+          <div className="pt-20">
+            <a
+              href="#"
+              title=""
+              className="inline-flex items-center justify-center px-8 py-3 text-base font-bold leading-7 text-gray-900 transition-all duration-200 bg-white border border-transparent rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
+              role="button"
+            >
+              Explore Now
+            </a>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
